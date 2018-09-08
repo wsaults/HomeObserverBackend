8ca7a634036f9a23c0dda28b69b353eca6a307cd
Will Saults <wsaults@yahoo.com>
2018-09-08 18:15:32 -0500

    initial project files

---
+++ src/main/java/com/saults/HomeObserverBackend/HomeObserverBackendApplication.java
@@ -0,0 +1,12 @@
+package com.saults.HomeObserverBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeObserverBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeObserverBackendApplication.class, args);
+	}
+}
---
+++ src/test/java/com/saults/HomeObserverBackend/HomeObserverBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package com.saults.HomeObserverBackend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HomeObserverBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
