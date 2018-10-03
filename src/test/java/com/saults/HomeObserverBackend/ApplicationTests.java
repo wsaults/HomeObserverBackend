d2d32ef08dc86abe7752203ecc3c8b86ecc1652f
Will Saults <wsaults@yahoo.com>
2018-10-02 21:43:44 -0500

    another test mod

--- src/test/java/com/saults/HomeObserverBackend/ApplicationTests.java
+++ src/test/java/com/saults/HomeObserverBackend/ApplicationTests.java
@@ -2,18 +2,13 @@ package com.saults.HomeObserverBackend;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 @RunWith(SpringRunner.class)
-@SpringBootTest
 public class ApplicationTests {
 
 	@Test
 	public void contextLoads() {
-		assertThat(true).isTrue();
+		assert(true);
 	}
-
 }
